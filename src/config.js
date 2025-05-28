fffd8dc972356a4bcb6174a8177aaaf942aeb620
Lorenz Equihua <panchocosa@gmail.com>
2025-05-28 00:42:34 -0600

    Set up GitHub Pages deployment workflow.
    Updated configuration to replace local API and images URLs with production URLs. Integrated `gh-pages` and added a workflow for deployment on GitHub Pages, ensuring seamless CI/CD for the project.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const API_URL = 'http://192.168.1.105:5000/graphql/';
-export const IMAGES_URL = "http://192.168.1.105:5000/"
+export const API_URL = 'http://144.172.98.81:8000/graphql';
+export const IMAGES_URL = "http://144.172.98.81:8000"
